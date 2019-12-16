4b006512856ebcc59f4fd7c9ddab8d6fe1c460f4
Takilios <48401581+Takilios@users.noreply.github.com>
2019-12-17 02:01:48 +0800

    Update OrderService.java

--- src/main/java/xmu/oomall/order/service/OrderService.java
+++ src/main/java/xmu/oomall/order/service/OrderService.java
@@ -36,6 +36,25 @@ public interface OrderService {
      */
     Object confirmOrder(Integer orderId);
 
+    
+     /**
+     * 用户查看自己的订单列表
+     * @param userId
+     * @param page
+     * @param limit
+     * @return OrderList
+     */
+    List<OrderPo> getOrderListByUserId(Integer userId,Integer page,Integer limit);
+
+    /**
+     * 用户查看订单详情
+     * @param id
+     * @return OrderItemList
+     */
+    List<OrderItem> getOrderDetails(Integer id);
+    
+    
+    
     /**
      * 订单单项退货
      * @param orderItem
